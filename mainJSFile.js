719b04e9986afe3701af52f0f0e852bdfb5a2dde
nazarbigun <nazar.bigun@gmail.com>
2016-07-20 21:27:47 +0300

    CSS changes

--- mainJSFile.js
+++ mainJSFile.js
@@ -1,20 +1,30 @@
-$(document).ready(function(){
-    $("#button").click(function(){
+$(document).ready(function() {
+    $("#button").click(function () {
         var toAdd = $('.input-area[name=checkListItem]').val();
         $('#myTasksList').append('<li> <div class = "tasks"> <button type="button" class="btn btn-default btn-lg active doneButton">&#9872</button> <textarea class ="form-control input-area task" rows="2">'
             + toAdd + '</textarea> <button type="button" class="btn btn-default btn-lg active starredButton">&#9734</button> </div> </li>');
-    })
-});
-
-$.getJSON('package.json', function(data) {
-    var output="";
-    for (var i in data.items) {
-        output += '<li> <div class = "tasks"> <button type="button" class="btn btn-default btn-lg active doneButton">&#9872</button> <textarea class ="form-control input-area task" rows="2">'
-            + data.items[i].text + '</textarea> <button type="button" class="btn btn-default btn-lg active starredButton">&#9734</button> </div> </li>';
-    }
-    document.getElementById("myTasksList").innerHTML = output;
+            //$.ajax({
+                //type: "POST",
+                //url: "package.json",
+                //dataType: 'JSON',
+                //data: {
+                //    'items.text': toAdd.toJSON,
+                //    'items.starred': 'false',
+                //    'items.done': 'false'
+                //}
+        });
     });
 
+    $.getJSON('package.json', function (data) {
+        var output = "";
+        for (var i in data.items) {
+            output += '<li> <div class = "tasks"> <button type="button" class="btn btn-default btn-lg active doneButton">&#9872</button> <textarea class ="form-control input-area task" rows="2">'
+                + data.items[i].text + '</textarea> <button type="button" class="btn btn-default btn-lg active starredButton">&#9734</button> </div> </li>';
+        }
+        ;
+        document.getElementById("myTasksList").innerHTML = output;
+    });
 
-
+    $("")
+});
 
